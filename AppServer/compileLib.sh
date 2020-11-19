af72c2b18b9df4b8132fbdf134de367beec9058e
jainyogesh <yogeshj.jain@gmail.com>
2020-11-19 11:39:32 +0530

    Adding library compile script

---
+++ AppServer/compileLib.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+compile () {
+javac -cp bin -d app1lib/  app1src/org/jainy/application/*.java
+javac -cp bin -d app2lib/  app2src/org/jainy/application/*.java
+}
+
+compile
+#java -cp bin org.jainy.server.AppServer -DclassLoader.childFirst=true app1=org.jainy.application.App1 app2=org.jainy.application.App2
